f4ff1d95fc30cef7f66ab6946dad4b2c7bbfd726
amr-daoud <amr.f.daoud@gmail.com>
2024-10-24 09:27:14 +0300

    Enable Cors

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<CentralizedFmsCloneContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IDashboardService, DashboardService>();
-
+builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+{
+    builder.AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed(origin => true) // allow any origin you can change here to allow localhost:4200
+            .AllowCredentials();
+}));
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,5 +25,6 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
 app.MapControllers();
 app.Run();
