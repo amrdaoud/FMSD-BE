d4e03cd9b517bdea8ae350caf4c67c8c436ff646
Saleem Kassab <saleemkassab5@gmail.com>
2024-10-30 15:25:20 +0300

    #FMS-3 Done

---
+++ Helper/Utilites.cs
@@ -0,0 +1,13 @@
+﻿namespace FMSD_BE.Helper
+{
+	public static class Utilites
+	{
+		public static DateTime convertDateToArabStandardDate(DateTime dateTime)
+		{
+			var timeZone = TimeZoneInfo.FindSystemTimeZoneById("Arab Standard Time");
+			dateTime = TimeZoneInfo.ConvertTimeFromUtc(dateTime, timeZone);
+
+			return dateTime;
+		}
+	}
+}
